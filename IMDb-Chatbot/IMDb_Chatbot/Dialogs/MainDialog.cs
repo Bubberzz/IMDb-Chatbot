eb64f2ebf69abcd0fb5a9b34f97a9985708fad2f
Stan Bubbers <stan.bubbers@live.co.uk>
2021-06-13 23:46:07 +0100

    Update: waterfall steps, added choice prompt Remove: unused code

--- IMDb-Chatbot/IMDb_Chatbot/Dialogs/MainDialog.cs
+++ IMDb-Chatbot/IMDb_Chatbot/Dialogs/MainDialog.cs
@@ -17,36 +17,34 @@ namespace IMDb_Chatbot.Dialogs
     public class MainDialog : ComponentDialog
     {
         private readonly FlightBookingRecognizer _luisRecognizer;
-        protected readonly ILogger Logger;
+        protected readonly ILogger _logger;
         private IImdbService _imdbService;
 
         // Dependency injection uses this constructor to instantiate MainDialog
         public MainDialog(
             FlightBookingRecognizer luisRecognizer, 
-            BookingDialog bookingDialog,
-            TopRatedMoviesDialog topRatedMoviesDialog,
-            TopRatedActorsDialog topRatedActorsDialog,
-            ComingSoonMoviesDialog comingSoonMoviesDialog,
-            MovieRouletteDialog movieRouletteDialog,
+            Dialog topRatedMoviesDialog,
+            Dialog topRatedActorsDialog,
+            Dialog comingSoonMoviesDialog,
+            Dialog movieRouletteDialog,
             ILogger<MainDialog> logger,
             IImdbService imdbService)
             : base(nameof(MainDialog))
         {
             _luisRecognizer = luisRecognizer;
-            Logger = logger;
+            _logger = logger;
             _imdbService = imdbService;
 
             AddDialog(new TextPrompt(nameof(TextPrompt)));
             AddDialog(new ChoicePrompt(nameof(ChoicePrompt)));
-            AddDialog(bookingDialog);
             AddDialog(topRatedMoviesDialog);
             //AddDialog(topRatedActorsDialog);
             //AddDialog(comingSoonMoviesDialog);
             //AddDialog(movieRouletteDialog);
             AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
             {
-                IntroStepAsync,
-                ActStepAsync,
+                SelectOptionAsync,
+                ReturnResultCard,
                 FinalStepAsync,
             }));
 
@@ -54,21 +52,37 @@ namespace IMDb_Chatbot.Dialogs
             InitialDialogId = nameof(WaterfallDialog);
         }
 
-        private async Task<DialogTurnResult> IntroStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private async Task<DialogTurnResult> SelectOptionAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            // Use the text provided in FinalStepAsync or the default if it is the first time.
-            var messageText = stepContext.Options?.ToString() ?? "What can I help you with today?\nSay something like \"Book a flight from Paris to Berlin on March 22, 2020\"";
-            var promptMessage = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput);
-            return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
+            if (stepContext.Context.Activity.Text == "Options")
+            {
+                var options = new PromptOptions()
+                {
+                    Prompt = MessageFactory.Text("Pick one of the following options"),
+                    RetryPrompt =
+                        MessageFactory.Text("That was not a valid choice, please one of the below options."),
+                    Choices = GetChoices(),
+                };
+
+                // Prompt the user with the configured PromptOptions.
+                return await stepContext.PromptAsync(nameof(ChoicePrompt), options, cancellationToken);
+            }
+
+            _logger.LogInformation("MainDialog.ChoiceCardStepAsync");
+            return await stepContext.NextAsync(cancellationToken: cancellationToken);
         }
 
-        private async Task<DialogTurnResult> ActStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private async Task<DialogTurnResult> ReturnResultCard(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
+            // Cards are sent as Attachments in the Bot Framework. Create a reply activity to return to the user.
+            var reply = MessageFactory.Attachment(new List<Attachment>());
 
-            //if (stepContext.Result is not null)
+            // Store user's text input in a variable
+            var input = stepContext.Context.Activity.Text;
+
             if (stepContext.Result is not null)
             {
-                switch (stepContext.Result)
+                switch (((FoundChoice)stepContext.Result).Value)
                 {
                     case "Top rated movies":
                         return await stepContext.BeginDialogAsync(nameof(TopRatedMoviesDialog), new TopRatedMoviesDialog(_imdbService), cancellationToken);
@@ -85,98 +99,33 @@ namespace IMDb_Chatbot.Dialogs
                 }
             }
 
-
-
-            if (!_luisRecognizer.IsConfigured)
-            {
-                // LUIS is not configured, we just run the BookingDialog path with an empty BookingDetailsInstance.
-                return await stepContext.BeginDialogAsync(nameof(BookingDialog), new BookingDetails(), cancellationToken);
-            }
-
-            // Call LUIS and gather any potential booking details. (Note the TurnContext has the response to the prompt.)
-            var luisResult = await _luisRecognizer.RecognizeAsync<FlightBooking>(stepContext.Context, cancellationToken);
-            switch (luisResult.TopIntent().intent)
-            {
-                case FlightBooking.Intent.BookFlight:
-                    await ShowWarningForUnsupportedCities(stepContext.Context, luisResult, cancellationToken);
-
-                    // Initialize BookingDetails with any entities we may have found in the response.
-                    var bookingDetails = new BookingDetails()
-                    {
-                        // Get destination and origin from the composite entities arrays.
-                        Destination = luisResult.ToEntities.Airport,
-                        Origin = luisResult.FromEntities.Airport,
-                        TravelDate = luisResult.TravelDate,
-                    };
-
-                    // Run the BookingDialog giving it whatever details we have from the LUIS call, it will fill out the remainder.
-                    return await stepContext.BeginDialogAsync(nameof(BookingDialog), bookingDetails, cancellationToken);
-
-                case FlightBooking.Intent.GetWeather:
-                    // We haven't implemented the GetWeatherDialog so we just display a TODO message.
-                    var getWeatherMessageText = "TODO: get weather flow here";
-                    var getWeatherMessage = MessageFactory.Text(getWeatherMessageText, getWeatherMessageText, InputHints.IgnoringInput);
-                    await stepContext.Context.SendActivityAsync(getWeatherMessage, cancellationToken);
-                    break;
-
-                default:
-                    // Catch all for unhandled intents
-                    var didntUnderstandMessageText = $"Sorry, I didn't get that. Please try asking in a different way (intent was {luisResult.TopIntent().intent})";
-                    var didntUnderstandMessage = MessageFactory.Text(didntUnderstandMessageText, didntUnderstandMessageText, InputHints.IgnoringInput);
-                    await stepContext.Context.SendActivityAsync(didntUnderstandMessage, cancellationToken);
-                    break;
-            }
+            // Send the card(s) to the user as an attachment to the activity
+            await stepContext.Context.SendActivityAsync(reply, cancellationToken);
 
             return await stepContext.NextAsync(null, cancellationToken);
         }
 
-        // Shows a warning if the requested From or To cities are recognized as entities but they are not in the Airport entity list.
-        // In some cases LUIS will recognize the From and To composite entities as a valid cities but the From and To Airport values
-        // will be empty if those entity values can't be mapped to a canonical item in the Airport.
-        private static async Task ShowWarningForUnsupportedCities(ITurnContext context, FlightBooking luisResult, CancellationToken cancellationToken)
-        {
-            var unsupportedCities = new List<string>();
-
-            var fromEntities = luisResult.FromEntities;
-            if (!string.IsNullOrEmpty(fromEntities.From) && string.IsNullOrEmpty(fromEntities.Airport))
-            {
-                unsupportedCities.Add(fromEntities.From);
-            }
-
-            var toEntities = luisResult.ToEntities;
-            if (!string.IsNullOrEmpty(toEntities.To) && string.IsNullOrEmpty(toEntities.Airport))
-            {
-                unsupportedCities.Add(toEntities.To);
-            }
-
-            if (unsupportedCities.Any())
-            {
-                var messageText = $"Sorry but the following airports are not supported: {string.Join(',', unsupportedCities)}";
-                var message = MessageFactory.Text(messageText, messageText, InputHints.IgnoringInput);
-                await context.SendActivityAsync(message, cancellationToken);
-            }
-        }
-
         private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            // If the child dialog ("BookingDialog") was cancelled, the user failed to confirm or if the intent wasn't BookFlight
-            // the Result here will be null.
-            if (stepContext.Result is BookingDetails result)
+           
+            // Give the user instructions about what to do next
+            await stepContext.Context.SendActivityAsync(
+                MessageFactory.Text("Type anything to do another search or type 'Options'"), cancellationToken);
+
+            return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+        }
+
+        private static IList<Choice> GetChoices()
+        {
+            var cardOptions = new List<Choice>()
             {
-                // Now we have all the booking details call the booking service.
+                new Choice() {Value = "Top rated movies", Synonyms = new List<string>() {"rated movies"}},
+                new Choice() {Value = "Top rated actors", Synonyms = new List<string>() {"actor", "actors", "rated actors"}},
+                new Choice() {Value = "Coming soon movies", Synonyms = new List<string>() {"coming", "soon"}},
+                new Choice() {Value = "IMDb Roulette", Synonyms = new List<string>() {"roulette", "imdb"}},
+            };
 
-                // If the call to the booking service was successful tell the user.
-
-                var timeProperty = new TimexProperty(result.TravelDate);
-                var travelDateMsg = timeProperty.ToNaturalLanguage(DateTime.Now);
-                var messageText = $"I have you booked to {result.Destination} from {result.Origin} on {travelDateMsg}";
-                var message = MessageFactory.Text(messageText, messageText, InputHints.IgnoringInput);
-                await stepContext.Context.SendActivityAsync(message, cancellationToken);
-            }
-
-            // Restart the main dialog with a different message the second time around
-            var promptMessage = "What else can I do for you?";
-            return await stepContext.ReplaceDialogAsync(InitialDialogId, promptMessage, cancellationToken);
+            return cardOptions;
         }
     }
 }
