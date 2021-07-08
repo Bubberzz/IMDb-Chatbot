8a147580746149379be5fb5df71d09a7d1684750
Stan Bubbers <stan.bubbers@live.co.uk>
2021-07-09 00:35:06 +0100

    Add: test cases + data generator to mock api calls

--- IMDb-Chatbot/IMDb_Chatbot.Tests/Common/BotTestBase.cs
+++ IMDb-Chatbot/IMDb_Chatbot.Tests/Common/BotTestBase.cs
@@ -1,7 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
@@ -11,11 +8,27 @@ using Xunit.Abstractions;
 
 namespace IMDb_Chatbot.Tests.Common
 {
-    /// <summary>
-    /// A base class with helper methods and properties to write bot tests.
-    /// </summary>
     public abstract class BotTestBase
     {
+        protected readonly string ExpectedTopRatedMoviesContent =
+            "Rating: 9.2 - The Shawshank Redemption\r\nRating: 9.1 - The Shawshank Redemption\r\nRating: 9 - The Shawshank Redemption\r\nRating: 9 - The Shawshank Redemption\r\nRating: 8.9 - The Shawshank Redemption\r\nRating: 8.9 - The Shawshank Redemption\r\nRating: 8.9 - The Shawshank Redemption\r\nRating: 8.8 - The Shawshank Redemption\r\nRating: 8.8 - The Shawshank Redemption\r\nRating: 8.8 - The Shawshank Redemption\r\n";
+
+        protected readonly string ExpectedTopRatedMoviesContentLastResult =
+            "Rating: 8.3 - The Shawshank Redemption\r\nRating: 8.3 - The Shawshank Redemption\r\nRating: 8.3 - The Shawshank Redemption\r\nRating: 8.3 - The Shawshank Redemption\r\nRating: 8.3 - The Shawshank Redemption\r\nRating: 8.3 - The Shawshank Redemption\r\nRating: 8.3 - The Shawshank Redemption\r\nRating: 8.3 - The Shawshank Redemption\r\nRating: 8.3 - The Shawshank Redemption\r\nRating: 8.3 - The Shawshank Redemption\r\n";
+        
+        protected readonly string ExpectedTopRatedActorsContent = 
+            "Rank: 1 - Sophia Di Martino\r\nRank: 2 - Sophia Di Martino\r\nRank: 3 - Sophia Di Martino\r\nRank: 4 - Sophia Di Martino\r\nRank: 5 - Sophia Di Martino\r\nRank: 6 - Sophia Di Martino\r\nRank: 7 - Sophia Di Martino\r\nRank: 8 - Sophia Di Martino\r\nRank: 9 - Sophia Di Martino\r\nRank: 10 - Sophia Di Martino\r\n";
+
+        protected readonly string ExpectedTopRatedActorsContentLastResult =
+            "Rank: 71 - Sophia Di Martino\r\nRank: 72 - Sophia Di Martino\r\nRank: 73 - Sophia Di Martino\r\nRank: 74 - Sophia Di Martino\r\nRank: 75 - Sophia Di Martino\r\nRank: 76 - Sophia Di Martino\r\nRank: 77 - Sophia Di Martino\r\nRank: 78 - Sophia Di Martino\r\nRank: 79 - Sophia Di Martino\r\nRank: 80 - Sophia Di Martino\r\n";
+
+        protected readonly string ExpectedComingSoonMoviesContent=
+            "- The Shawshank Redemption (1994) - Tim Robbins, Morgan Freeman\r\n- The Shawshank Redemption (1994) - Tim Robbins, Morgan Freeman\r\n- The Shawshank Redemption (1994) - Tim Robbins, Morgan Freeman\r\n- The Shawshank Redemption (1994) - Tim Robbins, Morgan Freeman\r\n- The Shawshank Redemption (1994) - Tim Robbins, Morgan Freeman\r\n- The Shawshank Redemption (1994) - Tim Robbins, Morgan Freeman\r\n- The Shawshank Redemption (1994) - Tim Robbins, Morgan Freeman\r\n- The Shawshank Redemption (1994) - Tim Robbins, Morgan Freeman\r\n- The Shawshank Redemption (1994) - Tim Robbins, Morgan Freeman\r\n- The Shawshank Redemption (1994) - Tim Robbins, Morgan Freeman\r\n";
+
+        protected readonly string ExpectedMoreResults =
+            "[{\"contentType\":\"application/vnd.microsoft.card.hero\",\"contentUrl\":null,\"content\":{\"title\":\"The Shawshank Redemption\",\"subtitle\":\"J.J. Carroll, Matt Tory, Jay Funsch\",\"text\":\"Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.\\r\\n Year: 2020 | Rating: 9.3 | Minutes: Unknown | Genre: Drama | Type: tvEpisode\",\"images\":[{\"url\":\"https://m.media-amazon.com/images/M/MV5BMmU2YTM5ZTMtYzBjMi00ZWVmLTlmNGMtYTY1YjEwMzc2MGEzXkEyXkFqcGdeQXVyNDE5MzY5MDc@._V1_.jpg\",\"alt\":null,\"tap\":null}],\"buttons\":[{\"type\":\"openUrl\",\"title\":\"Open IMDb\",\"image\":null,\"text\":null,\"displayText\":null,\"value\":\"https://www.imdb.com/title/tt12286264/\",\"channelData\":null,\"imageAltText\":null}],\"tap\":null},\"name\":null,\"thumbnailUrl\":null},{\"contentType\":\"application/vnd.microsoft.card.hero\",\"contentUrl\":null,\"content\":{\"title\":\"The Shawshank Redemption\",\"subtitle\":\"Ron Shahar, Alon Apple, Nitzan Avital\",\"text\":\"Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.\\r\\n Year: 2011 | Rating: 9.3 | Minutes: Unknown | Genre: Drama | Type: tvEpisode\",\"images\":[{\"url\":\"https://raw.githubusercontent.com/Bubberzz/IMDb-Chatbot/main/Images/not-found.jpg\",\"alt\":null,\"tap\":null}],\"buttons\":[{\"type\":\"openUrl\",\"title\":\"Open IMDb\",\"image\":null,\"text\":null,\"displayText\":null,\"value\":\"https://www.imdb.com/title/tt14308968/\",\"channelData\":null,\"imageAltText\":null}],\"tap\":null},\"name\":null,\"thumbnailUrl\":null},{\"contentType\":\"application/vnd.microsoft.card.hero\",\"contentUrl\":null,\"content\":{\"title\":\"The Shawshank Redemption: Behind the Scenes\",\"subtitle\":\"Frank Darabont, Morgan Freeman, Tim Robbins\",\"text\":\"Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.\\r\\n Year: 2004 | Rating: 9.3 | Minutes: 9 | Genre: Drama | Type: video\",\"images\":[{\"url\":\"https://raw.githubusercontent.com/Bubberzz/IMDb-Chatbot/main/Images/not-found.jpg\",\"alt\":null,\"tap\":null}],\"buttons\":[{\"type\":\"openUrl\",\"title\":\"Open IMDb\",\"image\":null,\"text\":null,\"displayText\":null,\"value\":\"https://www.imdb.com/title/tt5443386/\",\"channelData\":null,\"imageAltText\":null}],\"tap\":null},\"name\":null,\"thumbnailUrl\":null},{\"contentType\":\"application/vnd.microsoft.card.hero\",\"contentUrl\":null,\"content\":{\"title\":\"The Shawshank Redemption: Cast Interviews\",\"subtitle\":\"Clancy Brown, Morgan Freeman, Bob Gunton\",\"text\":\"Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.\\r\\n Year: 2004 | Rating: 9.3 | Minutes: 134 | Genre: Drama | Type: video\",\"images\":[{\"url\":\"https://raw.githubusercontent.com/Bubberzz/IMDb-Chatbot/main/Images/not-found.jpg\",\"alt\":null,\"tap\":null}],\"buttons\":[{\"type\":\"openUrl\",\"title\":\"Open IMDb\",\"image\":null,\"text\":null,\"displayText\":null,\"value\":\"https://www.imdb.com/title/tt5443390/\",\"channelData\":null,\"imageAltText\":null}],\"tap\":null},\"name\":null,\"thumbnailUrl\":null}]";
+
+
         // A lazy configuration object that gets instantiated once during execution when is needed
         private static readonly Lazy<IConfiguration> _configurationLazy = new Lazy<IConfiguration>(() =>
         {
@@ -28,21 +41,11 @@ namespace IMDb_Chatbot.Tests.Common
             return config.Build();
         });
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BotTestBase"/> class.
-        /// </summary>
         protected BotTestBase()
             : this(null)
         {
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BotTestBase"/> class.
-        /// </summary>
-        /// <param name="output">
-        /// An XUnit <see cref="ITestOutputHelper"/> instance.
-        /// See <see href="https://xunit.net/docs/capturing-output.html">Capturing Output</see> in the XUnit documentation for additional details.
-        /// </param>
         protected BotTestBase(ITestOutputHelper output)
         {
             Output = output;
@@ -52,14 +55,6 @@ namespace IMDb_Chatbot.Tests.Common
 
         protected ITestOutputHelper Output { get; }
 
-        /// <summary>
-        /// Test runners don't load environment variables defined in launchSettings.json
-        /// so this helper code loads it manually if the file is present.
-        /// This is useful to be able to have your own key files in your local machine without
-        /// having to put them in git.
-        /// If you use launch settings, make sure you set the Copy to Output Directory property to Copy Always.
-        /// </summary>
-        /// <param name="launchSettingsFile">The relative path to the launch settings file (i.e.: "Properties//launchSettings.json")</param>
         private static void LoadLaunchSettingsIntoEnvVariables(string launchSettingsFile)
         {
             if (!File.Exists(launchSettingsFile))
---
+++ IMDb-Chatbot/IMDb_Chatbot.Tests/Integration Tests/Dialogs/TestData/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using IMDb_Chatbot.Models;
+
+namespace IMDb_Chatbot.Tests.Integration_Tests.Dialogs.TestData
+{
+    public class TestCases
+    {
+        public List<TopRatedMovies.Root> TopRatedMovies { get; set; }
+        public List<TopRatedActors.Root> TopRatedActors { get; set; }
+        public AutoComplete.Root AutoCompleteMovie { get; set; }
+        public AutoComplete.Root AutoCompleteActor { get; set; }
+        public List<string> ComingSoonMovies { get; set; }
+        public ImdbSearch.Root MovieSearchResult { get; set; }
+        public Plot.PlotRoot Plot { get; set; }
+        public Rating.RatingRoot Rating { get; set; }
+        public List<string> Genre { get; set; }
+        public ImdbSearch.Root ActorSearchResult { get; set; }
+        public Bio.BioRoot Bio { get; set; }
+    }
+}
