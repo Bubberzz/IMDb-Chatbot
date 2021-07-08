d14ab55619cb93b195ac6b81b8979416fa5d951d
Stan Bubbers <stan.bubbers@live.co.uk>
2021-07-09 00:24:10 +0100

    Add: added a funtion to extract id's

---
+++ IMDb-Chatbot/IMDb_Chatbot/Services/ExtractResultId.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using IMDb_Chatbot.Models;
+
+namespace IMDb_Chatbot.Services
+{
+    public class ExtractResultId
+    {
+        public static string GetId(ImdbSearch.Root searchResult, int i = 0)
+        {
+            var extractId = searchResult.results[i].id.Split("/");
+            var resultId = extractId[2];
+            return resultId;
+        }
+
+        public static string GetId(List<TopRatedMovies.Root> searchResult, int i)
+        {
+            var extractId = searchResult[i].id.Split("/");
+            var resultId = extractId[2];
+            return resultId;
+        }
+
+        public static string GetId(List<TopRatedActors.Root> searchResult, int i)
+        {
+            var extractId = searchResult[i].id.Split("/");
+            var resultId = extractId[2];
+            return resultId;
+        }
+    }
+}
