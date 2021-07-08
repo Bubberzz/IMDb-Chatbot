2d85e598ce016dd4c52aafe88056e7cffe80f133
Stan Bubbers <stan.bubbers@live.co.uk>
2021-07-09 00:30:00 +0100

    Add: funtions to get actor and film details

---
+++ IMDb-Chatbot/IMDb_Chatbot/Services/GetActorDetails.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using IMDb_Chatbot.Interfaces;
+using IMDb_Chatbot.Models;
+
+namespace IMDb_Chatbot.Services
+{
+    public class GetActorDetails : IGetActorDetails
+    {
+        private static IImdbService _imdbService;
+
+        public GetActorDetails(IImdbService imdbService)
+        {
+            _imdbService = imdbService;
+        }
+
+        public async Task GetDetails(ImdbSearch.Root response, string resultId, int i = 0)
+        {
+            // get actor bio
+            var bio = await _imdbService.GetBio(resultId);
+            response.results[i].bio = bio.miniBios != null ? bio.miniBios[0].text : "";
+
+            // set birth date info
+            if (bio.birthDate is not null)
+            {
+                var birthPlace = bio.birthPlace ?? "Unknown";
+                try
+                {
+                    var formattedDate = DateTime.ParseExact($"{bio.birthDate}", "yyyy-mm-dd",
+                        null);
+                    response.results[i].born =
+                        formattedDate.ToString("dd MMMM yyyy") + " in " + birthPlace;
+                }
+                catch
+                {
+                    response.results[i].born = bio.birthDate + " in " + birthPlace;
+                }
+            }
+            else
+            {
+                response.results[i].born = "Unknown";
+            }
+
+            // set real name
+            response.results[i].realName = bio.realName ?? "";
+
+            // get actor rank
+            var rank = await _imdbService.AutoComplete(response.results[0].name);
+            response.results[i].actorRank = rank.d?[0].rank ?? 0;
+        }
+    }
+}
---
+++ IMDb-Chatbot/IMDb_Chatbot/Services/GetFilmDetails.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using IMDb_Chatbot.Interfaces;
+using IMDb_Chatbot.Models;
+
+namespace IMDb_Chatbot.Services
+{
+    public class GetFilmDetails : IGetFilmDetails
+    {
+        private static IImdbService _imdbService;
+
+        public GetFilmDetails(IImdbService imdbService)
+        {
+            _imdbService = imdbService;
+        }
+
+        public async Task GetDetails(ImdbSearch.Root response, string resultId, int i = 0)
+        {
+            // get plot and assign to film object
+            var plot = await _imdbService.GetPlot(resultId);
+            response.results[i].plot = plot.plots.Count != 0 ? plot.plots[0].text : "";
+
+            // get rating and assign to film object
+            var rating = await _imdbService.GetRating(resultId);
+            response.results[i].rating = rating.rating ?? "";
+
+            // get genre and assign to film object
+            var genre = await _imdbService.GetGenre(resultId);
+            response.results[i].genre = genre != null ? genre[0] : "";
+        }
+    }
+}
