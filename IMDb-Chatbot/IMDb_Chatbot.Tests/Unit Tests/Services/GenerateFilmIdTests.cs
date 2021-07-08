01303796ef80caf9caaef17d78d57e00cbed94e3
Stan Bubbers <stan.bubbers@live.co.uk>
2021-07-09 00:26:24 +0100

    Add: added a funtion to generate random id's for movie roulette + tests

---
+++ IMDb-Chatbot/IMDb_Chatbot.Tests/Unit Tests/Services/GenerateFilmIdTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using IMDb_Chatbot.Interfaces;
+using IMDb_Chatbot.Services;
+using Xunit;
+
+namespace IMDb_Chatbot.Tests.Unit_Tests.Services
+{
+    public class GenerateFilmIdTests
+    {
+        public IGenerateFilmId GenerateFilmId { get; }
+
+        public GenerateFilmIdTests()
+        {
+            GenerateFilmId = new GenerateFilmId();
+        }
+
+        [Fact]
+        public void GivenGenerateFilmId_ThenReturnRandomId()
+        {
+            // Act
+            var filmId = GenerateFilmId.FilmId();
+            var filmIdDigits = filmId[2..];
+
+            // Assert
+            Assert.Equal(9, filmId.Length);
+            Assert.Contains("tt", filmId);
+            Assert.True(filmId != null && filmId.Any(char.IsDigit));
+            Assert.True(filmIdDigits.All(char.IsDigit));
+        }
+    }
+}
---
+++ IMDb-Chatbot/IMDb_Chatbot/Services/GenerateFilmId.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using IMDb_Chatbot.Interfaces;
+
+namespace IMDb_Chatbot.Services
+{
+    public class GenerateFilmId : IGenerateFilmId
+    {
+        public string FilmId()
+        {
+            var random = new Random();
+            var resultId = "tt";
+            for (var i = 0; i < 7; i++)
+                resultId = string.Concat(resultId, random.Next(10).ToString());
+            return resultId;
+        }
+    }
+}
