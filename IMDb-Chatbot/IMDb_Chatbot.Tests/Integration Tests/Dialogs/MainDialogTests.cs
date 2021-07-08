89bd0fe193d4c73ee1ff06fcc9444a403cd62314
Stan Bubbers <stan.bubbers@live.co.uk>
2021-07-09 00:32:34 +0100

    Add: added a dialog that return films or actors based on user's search + tests

---
+++ IMDb-Chatbot/IMDb_Chatbot.Tests/Integration Tests/Dialogs/MainDialogTests.cs
@@ -0,0 +1,200 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using IMDb_Chatbot.Dialogs;
+using IMDb_Chatbot.Interfaces;
+using IMDb_Chatbot.Tests.Common;
+using IMDb_Chatbot.Tests.Integration_Tests.Dialogs.TestData;
+using Microsoft.Bot.Builder.Testing;
+using Microsoft.Bot.Builder.Testing.XUnit;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Newtonsoft.Json;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace IMDb_Chatbot.Tests.Integration_Tests.Dialogs
+{
+    public class MainDialogTests : BotTestBase
+    {
+        private readonly TopRatedMoviesDialog _mockTopRatedMoviesDialog;
+        private readonly TopRatedActorsDialog _mockTopRatedActorsDialog;
+        private readonly ComingSoonMoviesDialog _mockComingSoonMoviesDialog;
+        private readonly MovieRouletteDialog _mockMovieRouletteDialog;
+        private readonly ImdbSearchDialog _mockImdbSearchDialog;
+        private readonly IImdbService _mockImdbService;
+        private readonly Mock<ILogger<MainDialog>> _mockLogger;
+      
+        public MainDialogTests(ITestOutputHelper output)
+            : base(output)
+        {
+            _mockLogger = new Mock<ILogger<MainDialog>>();
+            _mockTopRatedMoviesDialog = new TopRatedMoviesDialog(_mockImdbService);
+            _mockTopRatedActorsDialog = new TopRatedActorsDialog(_mockImdbService);
+            _mockComingSoonMoviesDialog = new ComingSoonMoviesDialog(_mockImdbService);
+            _mockMovieRouletteDialog = new MovieRouletteDialog(_mockImdbService);
+            _mockImdbSearchDialog = new ImdbSearchDialog(_mockImdbService);
+            _mockImdbService = new Mock<IImdbService>().Object;
+        }
+
+
+        [Fact]
+        public async Task WhenUserRequestsOptions_ThenReturnOptions()
+        {
+            // Arrange
+            var sut = new MainDialog(_mockTopRatedMoviesDialog, _mockTopRatedActorsDialog, _mockComingSoonMoviesDialog, _mockMovieRouletteDialog, 
+                _mockImdbSearchDialog, _mockLogger.Object, _mockImdbService);
+
+            // Act
+            var testClient = new DialogTestClient(Channels.Msteams, sut);
+            
+            // Assert
+            var reply = await testClient.SendActivityAsync<IMessageActivity>("Options");
+            Assert.Equal("Pick one of the following options\n\n   1. Top rated movies\n   2. Top rated actors\n   3. Coming soon movies\n   4. IMDb Roulette",
+                reply.Text);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestDataGenerator.IMDbTestData), MemberType = typeof(TestDataGenerator))]
+        public async Task GivenMainDialog_WhenUserSearchesForMovie_ThenReturnHeroCardWithResult(TestDataObject testData)
+        {
+            // Arrange
+            var data = testData.GetObject<TestCases>();
+            var mock = new Mock<IImdbService>();
+            mock.Setup(x => x.GetSearchResult(It.IsAny<string>()))
+                .Returns(Task.FromResult(data.MovieSearchResult));
+            mock.Setup(x => x.GetPlot(It.IsAny<string>()))
+                .Returns(Task.FromResult(data.Plot));
+            mock.Setup(x => x.GetRating(It.IsAny<string>()))
+                .Returns(Task.FromResult(data.Rating));
+            mock.Setup(x => x.GetGenre(It.IsAny<string>()))
+                .Returns(Task.FromResult(data.Genre));
+
+            var heroCard = new HeroCard
+            {
+                Title = "The Shawshank Redemption",
+                Subtitle = "Tim Robbins, Morgan Freeman, Bob Gunton",
+                Text = "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.\r\n Year: 1994 | Rating: 9.3 | Genre: Drama | Minutes: 142 | Type: movie",
+                Images = new List<CardImage> { new CardImage("https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg") },
+                Buttons = new List<CardAction>
+                {
+                    new(ActionTypes.OpenUrl, "Open IMDb",
+                        value: "https://www.imdb.com/title/tt0111161/"),
+                    new(ActionTypes.MessageBack, "More Results",
+                        value: "Show More: Movie Results")
+                },
+            };
+
+            // Act
+            var sut = new MainDialog(_mockTopRatedMoviesDialog, _mockTopRatedActorsDialog, _mockComingSoonMoviesDialog, _mockMovieRouletteDialog,
+                _mockImdbSearchDialog, _mockLogger.Object, mock.Object);
+            var testClient = new DialogTestClient(Channels.Msteams, sut);
+
+            // Assert
+            var reply = await testClient.SendActivityAsync<IMessageActivity>("The Shawshank Redemption");
+            Assert.NotNull(reply.Attachments[0]);
+            Assert.Equal("list", reply.AttachmentLayout);
+            Assert.Equal("Microsoft.Bot.Schema.HeroCard", reply.Attachments[0].Content.ToString());
+
+            var expected = JsonConvert.SerializeObject(heroCard);
+            var actual = JsonConvert.SerializeObject(reply.Attachments[0].Content);
+            Assert.Equal(expected, actual);
+
+            reply = testClient.GetNextReply<IMessageActivity>();
+            Assert.Equal("Type anything to do another search or type 'Options'", reply.Text);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestDataGenerator.IMDbTestData), MemberType = typeof(TestDataGenerator))]
+        public async Task GivenMainDialog_WhenUserSelectsMoreResults_ThenReturnHeroCards(TestDataObject testData)
+        {
+            // Arrange
+            var data = testData.GetObject<TestCases>();
+            var mock = new Mock<IImdbService>();
+            mock.Setup(x => x.GetSearchResult(It.IsAny<string>()))
+                .Returns(Task.FromResult(data.MovieSearchResult));
+            mock.Setup(x => x.AutoComplete(It.IsAny<string>()))
+                .Returns(Task.FromResult(data.AutoCompleteMovie));
+            mock.Setup(x => x.GetPlot(It.IsAny<string>()))
+                .Returns(Task.FromResult(data.Plot));
+            mock.Setup(x => x.GetRating(It.IsAny<string>()))
+                .Returns(Task.FromResult(data.Rating));
+            mock.Setup(x => x.GetGenre(It.IsAny<string>()))
+                .Returns(Task.FromResult(data.Genre));
+
+            // Act
+            var sut = new MainDialog(_mockTopRatedMoviesDialog, _mockTopRatedActorsDialog, _mockComingSoonMoviesDialog, _mockMovieRouletteDialog,
+                _mockImdbSearchDialog, _mockLogger.Object, mock.Object);
+            var testClient = new DialogTestClient(Channels.Msteams, sut);
+
+            // Assert
+            var reply = await testClient.SendActivityAsync<IMessageActivity>("The Shawshank Redemption");
+            reply = testClient.GetNextReply<IMessageActivity>();
+            Assert.Equal("Type anything to do another search or type 'Options'", reply.Text);
+
+            var activity = new Activity(value: "Show More: Movie Results");
+            var activityReply = testClient.SendActivityAsync<IActivity>(activity);
+            reply = testClient.GetNextReply<IMessageActivity>();
+
+            Assert.NotNull(reply.Attachments[0]);
+            Assert.Equal("carousel", reply.AttachmentLayout);
+            Assert.Equal("Microsoft.Bot.Schema.HeroCard", reply.Attachments[0].Content.ToString());
+
+            var actual = JsonConvert.SerializeObject(reply.Attachments);
+            Assert.Equal(ExpectedMoreResults, actual);
+
+            reply = testClient.GetNextReply<IMessageActivity>();
+            Assert.Equal("Type anything to do another search or type 'Options'", reply.Text);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestDataGenerator.IMDbTestData), MemberType = typeof(TestDataGenerator))]
+        public async Task GivenMainDialog_WhenUserSearchesForActor_ThenReturnHeroCardWithResult(TestDataObject testData)
+        {
+            // Arrange
+            var data = testData.GetObject<TestCases>();
+            var mock = new Mock<IImdbService>();
+            mock.Setup(x => x.GetSearchResult(It.IsAny<string>()))
+                .Returns(Task.FromResult(data.ActorSearchResult));
+            mock.Setup(x => x.AutoComplete(It.IsAny<string>()))
+                .Returns(Task.FromResult(data.AutoCompleteActor));
+            mock.Setup(x => x.GetBio(It.IsAny<string>()))
+                .Returns(Task.FromResult(data.Bio));
+
+            var heroCard = new HeroCard
+            {
+                Title = "Sophia Di Martino",
+                Subtitle = "IMDb rank: 2",
+                Text = " was born on 15 January 1983 in Nottingham, Nottinghamshire, England, UK\r\nSophia Di Martino was born on November 15, 1983 in Nottingham, Nottinghamshire, England. She is an actress and director, known for Flowers (2016), Loki (2021) and Yesterday (2019).\r\n Known for: Flowers, Loki, Yesterday, Into the Badlands\r\n",
+                Images = new List<CardImage> { new("https://m.media-amazon.com/images/M/MV5BYjk4YmMyOWYtMzlhZS00Yjg4LTk0NjQtNDdmMmViMzk3MDZhXkEyXkFqcGdeQXVyMjQwMDg0Ng@@._V1_.jpg") },
+                Buttons = new List<CardAction>
+                {
+                    new(ActionTypes.OpenUrl, "Open IMDb",
+                        value: "https://www.imdb.com/name/nm1620545/")
+                },
+            };
+
+            // Act
+            var sut = new MainDialog(_mockTopRatedMoviesDialog, _mockTopRatedActorsDialog, _mockComingSoonMoviesDialog, _mockMovieRouletteDialog,
+                _mockImdbSearchDialog, _mockLogger.Object, mock.Object);
+            var testClient = new DialogTestClient(Channels.Msteams, sut);
+
+            // Assert
+            var reply = await testClient.SendActivityAsync<IMessageActivity>("Will Smith");
+            Assert.NotNull(reply.Attachments[0]);
+            Assert.Equal("list", reply.AttachmentLayout);
+            Assert.Equal("Microsoft.Bot.Schema.HeroCard", reply.Attachments[0].Content.ToString());
+
+            var expected = JsonConvert.SerializeObject(heroCard);
+            var actual = JsonConvert.SerializeObject(reply.Attachments[0].Content);
+            Assert.Equal(expected, actual);
+
+            reply = testClient.GetNextReply<IMessageActivity>();
+            Assert.Equal("Type anything to do another search or type 'Options'", reply.Text);
+        }
+    }
+}
---
+++ IMDb-Chatbot/IMDb_Chatbot/Dialogs/ImdbSearchDialog.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using IMDb_Chatbot.Interfaces;
+using IMDb_Chatbot.Models;
+using IMDb_Chatbot.Services;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+
+namespace IMDb_Chatbot.Dialogs
+{
+    public class ImdbSearchDialog : CancelAndHelpDialog
+    {
+        private static IImdbService _imdbService;
+        private readonly IGetFilmDetails _getFilmDetails;
+        private readonly IGetActorDetails _getActorDetails;
+        private const string ShowMoreText = "Show More: Movie Results";
+        private const string IMDbReoulette = "IMDb Roulette";
+        private ImdbSearch.Root _response;
+
+
+        public ImdbSearchDialog(IImdbService imdbService)
+            : base(nameof(ImdbSearchDialog))
+        {
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new ConfirmPrompt(nameof(ConfirmPrompt)));
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
+            {
+                ReturnSingleSearch,
+                ReturnMultipleSearches,
+                ReturnSearchForIMDbRoulette
+            }));
+
+            InitialDialogId = nameof(WaterfallDialog);
+            _imdbService = imdbService;
+            _getFilmDetails = new GetFilmDetails(_imdbService);
+            _getActorDetails = new GetActorDetails(_imdbService);
+            _response = new ImdbSearch.Root();
+        }
+
+        private async Task<DialogTurnResult> ReturnSingleSearch(WaterfallStepContext stepContext,
+            CancellationToken cancellationToken)
+        {
+            if (stepContext.Context.Activity.Text == IMDbReoulette ||
+                stepContext.Context.Activity.Value.ToString() == ShowMoreText)
+            {
+                return await stepContext.NextAsync(null, cancellationToken);
+            }
+
+            var search = stepContext.Context.Activity.Text.Replace(" ", "%20");
+            _response = await _imdbService.GetSearchResult(search);
+
+            if (_response.results is null)
+            {
+                await stepContext.Context.SendActivityAsync(
+                    MessageFactory.Text(@"Sorry I couldn't find anything ¯\_(ツ)_/¯"),
+                    cancellationToken);
+                await stepContext.Context.SendActivityAsync(MessageFactory.Text("Please try again"),
+                    cancellationToken);
+                return await stepContext.NextAsync(null, cancellationToken);
+            }
+
+            var resultId = ExtractResultId.GetId(_response);
+
+            if (_response.results[0].id.StartsWith("/title"))
+            {
+                await _getFilmDetails.GetDetails(_response, resultId);
+            }
+
+            if (_response.results[0].id.StartsWith("/name"))
+            {
+                await _getActorDetails.GetDetails(_response, resultId);
+            }
+
+            var card = Cards.GetCard(_response, true, false);
+            var reply = MessageFactory.Attachment(new List<Attachment>()
+            {
+                card[0].ToAttachment()
+            });
+
+            // Send the card(s) to the user as an attachment to the activity
+            await stepContext.Context.SendActivityAsync(reply, cancellationToken);
+            return await stepContext.NextAsync(null, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> ReturnMultipleSearches(WaterfallStepContext stepContext,
+            CancellationToken cancellationToken)
+        {
+            if (stepContext.Context.Activity.Value.ToString() != ShowMoreText)
+            {
+                return await stepContext.NextAsync(null, cancellationToken);
+            }
+
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text("Loading.."),
+                cancellationToken);
+
+            for (var i = 1; i < _response.results.Count; i++)
+            {
+                var resultId = ExtractResultId.GetId(_response, i);
+
+                if (_response.results[i].id.StartsWith("/title"))
+                {
+                    await _getFilmDetails.GetDetails(_response, resultId, i);
+                }
+
+                if (_response.results[i].id.StartsWith("/name"))
+                {
+                    await _getActorDetails.GetDetails(_response, resultId, i);
+                }
+            }
+
+            var cards = Cards.GetCard(_response, false, false);
+            var reply = MessageFactory.Attachment(new List<Attachment>());
+            reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+
+            for (var i = 1; i < cards.Count; i++)
+            {
+                reply.Attachments.Add(cards[i].ToAttachment());
+            }
+
+            // Send the card(s) to the user as an attachment to the activity
+            await stepContext.Context.SendActivityAsync(reply, cancellationToken);
+            return await stepContext.NextAsync(null, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> ReturnSearchForIMDbRoulette(WaterfallStepContext stepContext,
+            CancellationToken cancellationToken)
+        {
+            if (stepContext.Context.Activity.Text != IMDbReoulette)
+            {
+                return await stepContext.NextAsync(null, cancellationToken);
+            }
+
+            var search = stepContext.Context.Activity.Value.ToString()?.Replace(" ", "%20");
+
+            // Remove characters that can cause problems
+            var charsToRemove = new string[] {"@", "#"};
+            search = charsToRemove.Aggregate(search, (current, c) => current.Replace(c, string.Empty));
+            var response = await _imdbService.GetSearchResult(search);
+
+
+            if (response.results is null)
+            {
+                await stepContext.Context.SendActivityAsync(
+                    MessageFactory.Text(@"Sorry I couldn't find anything ¯\_(ツ)_/¯"),
+                    cancellationToken);
+                await stepContext.Context.SendActivityAsync(MessageFactory.Text("Please try again"),
+                    cancellationToken);
+                return await stepContext.NextAsync(null, cancellationToken);
+            }
+
+            var resultId = ExtractResultId.GetId(response);
+            await _getFilmDetails.GetDetails(response, resultId);
+            return await stepContext.NextAsync(response, cancellationToken);
+        }
+    }
+}
