2f6a8b23c41f0d6d61323ee47b0d8feb332cd095
Stan Bubbers <stan.bubbers@live.co.uk>
2021-07-09 00:44:13 +0100

    Add: user response list for the recommend dialog

---
+++ IMDb-Chatbot/IMDb_Chatbot/Services/UserResponse.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace IMDb_Chatbot.Services
+{
+    public class UserResponse
+    {
+        public static List<string> UserResponseNegative()
+        {
+            var negativeResponse = new List<string>
+            {
+                "No",
+                "no",
+                "Nah",
+                "nah",
+                "Nope",
+                "nope"
+            };
+            return negativeResponse;
+        }
+
+        public static List<string> UserResponsePositive()
+        {
+            var positiveResponse = new List<string>
+            {
+                "Yes",
+                "yes",
+                "ok",
+                "Ok",
+                "OK",
+                "Sure",
+                "sure",
+                "Yeah",
+                "yeah",
+                "ye",
+                "Ye",
+                "Yea",
+                "yea"
+            };
+            return positiveResponse;
+        }
+    }
+}
