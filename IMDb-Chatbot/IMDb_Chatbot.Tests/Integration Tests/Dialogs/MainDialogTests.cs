1abcf33c31448db2d9f7655f50c6f99c3696e83d
Stan Bubbers <33037666+Bubberzz@users.noreply.github.com>
2021-07-09 11:16:15 +0100

    Update MainDialogTests.cs

--- IMDb-Chatbot/IMDb_Chatbot.Tests/Integration Tests/Dialogs/MainDialogTests.cs
+++ IMDb-Chatbot/IMDb_Chatbot.Tests/Integration Tests/Dialogs/MainDialogTests.cs
@@ -1,6 +1,3 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using IMDb_Chatbot.Dialogs;
